6829201c96b7b366afc66cdc8d437a87d95dd3cf
Ramtin <Ramtin.abadi@gmail.com>
2025-11-29 20:10:44 +0100

    Improved the description of the root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -10,21 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gomore",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "CLI for go.yaml",
+	Long: `The management CLI for initiating, parsing, and common functionalities of go.yaml
+	
+go.yaml acts as the central config file for your Go project`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +35,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
