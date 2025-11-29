ab2c76e11cd12d551dcd50aa09007ae27f7640ba
Ramtin <Ramtin.abadi@gmail.com>
2025-11-29 20:11:12 +0100

    Improved the implementation of GoYAML

--- goyaml/goyaml.go
+++ goyaml/goyaml.go
@@ -5,69 +5,46 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/vieolo/termange"
 	"gopkg.in/yaml.v2"
 )
 
 type GoYAML struct {
-	Name        string
-	Description string
-	Version     string
-	Commands    map[string]string
-	Config      map[string]any
+	// The name of the project. The name should be the human-readable name and not
+	// the git path. e.g., gomore
+	Name string `yaml:"name"`
+	// [Optional] description of the project
+	Description string `yaml:"description"`
+	// version of the project
+	Version string `yaml:"version"`
+	// Pre-defined commands in a key-value pair format
+	Commands map[string]string `yaml:"commands"`
 }
 
+// Prints the list of the pre-defined commands, if any
+func (y GoYAML) PrintCommandList(title string) {
+	if len(y.Commands) == 0 {
+		return
+	}
+
+	termange.PrintInfoln(title)
+	for k := range y.Commands {
+		fmt.Printf(" - %s\n", k)
+	}
+}
+
+// Reads and parses the go.yaml file
 func ReadGoYAML() (GoYAML, error) {
 	b, readErr := os.ReadFile("go.yaml")
 	if readErr != nil {
 		return GoYAML{}, errors.New("go.yaml file could not found")
 	}
 
-	var raw map[string]any
+	var raw GoYAML
 	parseErr := yaml.Unmarshal(b, &raw)
 	if parseErr != nil {
-		return GoYAML{}, errors.New("go.yaml could not be parsed")
+		return GoYAML{}, parseErr
 	}
 
-	name, nameErr := getString(raw, "name", false)
-	if nameErr != nil {
-		return GoYAML{}, nameErr
-	}
-
-	description, descriptionErr := getString(raw, "description", true)
-	if descriptionErr != nil {
-		return GoYAML{}, descriptionErr
-	}
-
-	version, versionErr := getString(raw, "version", false)
-	if versionErr != nil {
-		return GoYAML{}, versionErr
-	}
-
-	goyaml := GoYAML{
-		Name:        name,
-		Description: description,
-		Version:     version,
-	}
-	for k, v := range raw {
-		fmt.Println(k, v)
-	}
-
-	return goyaml, nil
-}
-
-func getString(raw map[string]any, key string, optional bool) (string, error) {
-	val, exist := raw[key]
-	if !exist {
-		if optional {
-			return "", nil
-		}
-		return "", fmt.Errorf("%s is missing", key)
-	}
-
-	s, isString := val.(string)
-	if !isString {
-		return "", fmt.Errorf("%s is not a string", key)
-	}
-
-	return s, nil
+	return raw, nil
 }
